c35399cedc943970e8c6af71f2ad4586cb9035da
Jan Gleixner <jan.gleixner@gmail.com>
2023-06-29 11:38:00 +0200

    simplify using yield from

--- fair.py
+++ fair.py
@@ -1,62 +1,67 @@
 import random
+import logging
 
-
-def unfair():
+def unfair() -> bool:
+    """Generates an unfair coin toss."""
     i = 0
     while True:
         i = i + 1
-        out = random.choice((True, False))  # True,
+        out = random.choice((True, False))
         if not i % 100000:
-            print(i, "th unfair toss:", out)
+            logging.info(f"{i}th unfair toss: {out}")
         yield out
 
-
-def fair():
-    a = yield
-    b = yield
-    next_level = fair()
-    next(next_level)
-    emitted = fair()
-    next(emitted)
+def push_and_forward(coroutine, value):
+    """Pushes a value into a coroutine and yields all its output neccessary to do so."""
     while True:
-        if a == b:
-            while True:
-                next_level_out = next_level.send(a)
-                if next_level_out is None:
-                    break
-                else:
-                    yield next_level_out
+        result = coroutine.send(value)
+        if result is not None:
+            yield result
         else:
-            yield a
+            break
 
-        while True:
-            emitted_out = emitted.send(a != b)
-            if emitted_out is None:
-                break
-            else:
-                yield emitted_out
+def push_all_and_forward(coroutine, generator):
+    """Pushes all values from a generator into a coroutine and yields its output."""
+    for i in generator:
+        yield from push_and_forward(coroutine, i)
 
-        a = yield
-        b = yield
+def fair_maker():
+    """Generates a fair coin toss from an unfair source."""
+    coin_toss_1 = yield
+    coin_toss_2 = yield
 
+    are_equal_fair_maker = fair_maker()
+    next(are_equal_fair_maker)
+    
+    which_if_equal_fair_maker = fair_maker()
+    next(which_if_equal_fair_maker)
 
-def fair_gen(g):
-    fair_ = fair()
-    next(fair_)
     while True:
-        next_in = next(g)
-        while True:
-            next_out = fair_.send(next_in)
-            if next_out is None:
-                break
-            else:
-                yield next_out
+        # P(coin_toss_1,coin_toss_2) can be decomposed into  P(coint_toss_1 | coin_toss_1=coin_toss_2) P(coin_toss_1=coin_toss_2) 
+        if coin_toss_1 == coin_toss_2:
+            yield from push_and_forward(which_if_equal_fair_maker, coin_toss_1)
+        else:
+            yield coin_toss_1 # Given that "Heads,Tails" is as likely as "Tails,Heads" this is a fair coin toss
 
+        yield from push_and_forward(are_equal_fair_maker, coin_toss_1 == coin_toss_2)
 
-fair_coin = fair_gen(unfair())
-j = 0
-for e in range(10000000):
-    j = j + 1
-    fc = next(fair_coin)
-    if not j % 100000:
-        print(j, "th fair toss:", fc)
+        coin_toss_1 = yield
+        coin_toss_2 = yield
+
+def make_fair(generator):
+    """Makes an unfair generator fair."""
+    fair = fair_maker()
+    next(fair)
+    yield from push_all_and_forward(fair, generator)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    
+    fair_coin = make_fair(unfair())
+
+    j = 0
+    for _ in range(10000000):
+        j = j + 1
+        fair_toss = next(fair_coin)
+        if not j % 100000:
+            logging.info(f"{j}th fair toss: {fair_toss}")
